640cc637d5c76641d3572206ac1cf7af8031a651
pradoshcodes <pradoshmohanty034@gmail.com>
2024-09-04 23:09:22 +0530

    Implement API call functionality

--- src/main/java/com/javaguide/Springboot_backend/controller/EmployeeController.java
+++ src/main/java/com/javaguide/Springboot_backend/controller/EmployeeController.java
@@ -33,4 +33,16 @@ public class EmployeeController {
         Employee employee=employeeRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Employee not exist with id:"+id));
         return ResponseEntity.ok(employee);
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id,Employee employeeDetails){
+        Employee updateEmployee=employeeRepository .findById(id).orElseThrow(()->new ResourceNotFoundException("Employee not exist with id:" + id));
+
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+        return ResponseEntity.ok(updateEmployee);
+    }
 }
