382cf8ab62d7b2d3138e3c8bdf9dbd3c88004dda
pradoshcodes <pradoshmohanty034@gmail.com>
2024-09-06 00:13:44 +0530

    Implement API call functionality

--- src/main/java/com/javaguide/Springboot_backend/controller/EmployeeController.java
+++ src/main/java/com/javaguide/Springboot_backend/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.javaguide.Springboot_backend.exception.ResourceNotFoundException;
 import com.javaguide.Springboot_backend.model.Employee;
 import com.javaguide.Springboot_backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,8 +36,9 @@ public class EmployeeController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable long id,Employee employeeDetails){
-        Employee updateEmployee=employeeRepository .findById(id).orElseThrow(()->new ResourceNotFoundException("Employee not exist with id:" + id));
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetails){
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
 
         updateEmployee.setFirstName(employeeDetails.getFirstName());
         updateEmployee.setLastName(employeeDetails.getLastName());
@@ -45,4 +47,8 @@ public class EmployeeController {
         employeeRepository.save(updateEmployee);
         return ResponseEntity.ok(updateEmployee);
     }
+
+//    public ResponseEntity<HttpStatus> deleteEmployee(long id){
+//        Employee employee=employeeRepository.findById(id).orElseThrow()f
+//    }
 }
